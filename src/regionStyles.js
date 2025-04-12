ade825524ce0c56066b9de6abab4613703883b25
Caleb2334432 <105329729+Caleb2334432@users.noreply.github.com>
2025-04-11 22:28:10 -0500

    Add files via upload

---
+++ src/regionStyles.js
@@ -0,0 +1,8 @@
+
+export const regionStyles = [
+  { top: "20%", left: "30%", width: "8%", height: "8%", backgroundColor: "red" },
+  { top: "30%", left: "45%", width: "8%", height: "8%", backgroundColor: "orange" },
+  { top: "40%", left: "35%", width: "8%", height: "8%", backgroundColor: "yellow" },
+  { top: "50%", left: "50%", width: "8%", height: "8%", backgroundColor: "green" },
+  { top: "60%", left: "40%", width: "8%", height: "8%", backgroundColor: "blue" },
+];
